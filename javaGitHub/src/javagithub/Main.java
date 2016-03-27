e17e34688dc8ba3ea42ff144e9196dc32c69efe9
Mavalfer <mvalfer92@gmail.com>
2016-03-28 01:41:10 +0200

    Fix syntax errors

--- javaGitHub/src/javagithub/Main.java
+++ javaGitHub/src/javagithub/Main.java
@@ -2,10 +2,49 @@ package javagithub;
 
 
 import java.sql.*;
-
-
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
+
+    public static boolean chkComercio(Statement stmt, String comercio) {
+        boolean chk = false;
+        try {
+            ResultSet rs = stmt.executeQuery("SELECT nombre from MVComercio");
+            while (rs.next()){
+                if(rs.getString("nombre").equals(comercio)){
+                    chk = true;
+                    break;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return chk;
+    }
+
+    public static boolean chkFecha(Statement stmt, String comercio) {
+        boolean abierto = false;
+        LocalDate fecha = LocalDate.now();
+        String dia = fecha.getDayOfWeek().toString();  //OJO
+        try {
+            ResultSet rsID = stmt.executeQuery("SELECT id_mvcomercio FROM MVComercio"
+                    + "WHERE nombre = '" + comercio + "'");
+            ResultSet rsFecha = stmt.executeQuery("SELECT hInicio, hFIn FROM MVHorario"
+                    + "WHERE " + String.valueOf(rsID.getInt("id_comercio"))
+                    + "AND dia = '" + dia + "'");
+            LocalTime horaLocal = LocalTime.now();
+            Time hora = Time.valueOf(horaLocal);
+            if (hora.after(rsFecha.getTime("hInicio")) && hora.before(rsFecha.getTime("hFin"))) {
+                abierto = true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return abierto;
+    }
 
     public static void main(String[] args) {
 
@@ -34,25 +73,60 @@ public class Main {
             String insertc2 = "INSERT INTO MVComercio VALUES (278,'El pollazo','Calle falsa 123','Polleria')";
             String insertc1h1 = "INSERT INTO MVHorario VALUES (11,'Lunes','08:00:00','20:30:00','156')";
             String insertc1h2 = "INSERT INTO MVHorario VALUES (12,'Martes','08:00:00','20:30:00','156')";
-            String insertc1h3 = "INSERT INTO MVHorario VALUES (13,'Miercoles','08:00:00','20:30:00','156')";
+            String insertc1h3 = "INSERT INTO MVHorario VALUES (13,'Miércoles','08:00:00','20:30:00','156')";
             String insertc1h4 = "INSERT INTO MVHorario VALUES (14,'Jueves','08:00:00','20:30:00','156')";
             String insertc1h5 = "INSERT INTO MVHorario VALUES (15,'Viernes','08:00:00','20:30:00','156')";
             String insertc1h6 = "INSERT INTO MVHorario VALUES (16,'Sabado','17:00:00','20:30:00','156')";
             String insertc1h7 = "INSERT INTO MVHorario VALUES (17,'Domingo',null,null,'156')";
             String insertc2h1 = "INSERT INTO MVHorario VALUES (21,'Lunes','08:00:00','20:30:00','278')";
             String insertc2h2 = "INSERT INTO MVHorario VALUES (22,'Martes','08:00:00','20:30:00','278')";
-            String insertc2h3 = "INSERT INTO MVHorario VALUES (23,'Miercoles','08:00:00','20:30:00','278')";
+            String insertc2h3 = "INSERT INTO MVHorario VALUES (23,'Miércoles','08:00:00','20:30:00','278')";
             String insertc2h4 = "INSERT INTO MVHorario VALUES (24,'Jueves','08:00:00','20:30:00','278')";
             String insertc2h5 = "INSERT INTO MVHorario VALUES (25,'Viernes','08:00:00','20:30:00','278')";
             String insertc2h6 = "INSERT INTO MVHorario VALUES (26,'Sabado','17:00:00','20:30:00','278')";
             String insertc2h7 = "INSERT INTO MVHorario VALUES (27,'Domingo',null,null,'278')";
             Statement stmt = null;
+            
+            ArrayList<String> inserciones = new ArrayList();
+            inserciones.add(insertc1);
+            inserciones.add(insertc2);
+            inserciones.add(insertc1h1);
+            inserciones.add(insertc1h2);
+            inserciones.add(insertc1h3);
+            inserciones.add(insertc1h4);
+            inserciones.add(insertc1h5);
+            inserciones.add(insertc1h6);
+            inserciones.add(insertc1h7);
+            inserciones.add(insertc2h1);
+            inserciones.add(insertc2h2);
+            inserciones.add(insertc2h3);
+            inserciones.add(insertc2h4);
+            inserciones.add(insertc2h5);
+            inserciones.add(insertc2h6);
+            inserciones.add(insertc2h7);
 
             try {
                 stmt = con.createStatement();
                 stmt.executeUpdate(dropTables);
                 stmt.executeUpdate(sqlMVComercio);
                 stmt.executeUpdate(sqlMVHorario);
+                for(int i = 0; i < inserciones.size(); i++){
+                    stmt.executeUpdate(inserciones.get(i));
+                }
+                Scanner sc = new Scanner(System.in);
+                String comercio;
+                do{
+                    comercio = sc.nextLine();
+                    if (chkComercio(stmt, comercio)){
+                        if (chkFecha(stmt, comercio)){
+                            System.out.println("Abierto");
+                        }else{
+                            System.out.println("Cerrado");
+                        }
+                    }else{
+                        System.out.println("Nombre incorrecto");
+                    }
+                }while(!chkComercio(stmt, comercio));
             /*    ResultSet rs = stmt.executeQuery(insertc1);
                 while (rs.next()) {
                     
